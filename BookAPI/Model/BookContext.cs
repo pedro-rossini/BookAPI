9257f24c84ab9d26254feab266e69f4374b923b6
Pedro Rossini <pedro-rossini@hotmail.com>
2022-09-14 11:09:11 -0300

    Primeiro commit

---
+++ BookAPI/Model/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace BookAPI.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
